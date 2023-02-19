9863efeb7359fd4247f94a1d780a95b07316b2b5
Michał Prądzyński <41488514+michalPradzynski@users.noreply.github.com>
2023-02-19 16:46:26 +0100

    tictactoe_ai stage 2/5

--- tictactoe_ai.py
+++ tictactoe_ai.py
@@ -1,7 +1,12 @@
+import random
 from typing import Any
 
 GAME_SYMBOLS: set[str] = {"X", "O", "_"}
 STARTING_TABLE_LEN: int = 9
+
+
+class WrongCoordinates(ValueError):
+    pass
 
 
 def game_table(input_matrix: list[list[str]]) -> str:
@@ -20,9 +25,9 @@ def game_table(input_matrix: list[list[str]]) -> str:
     """)
 
 
-def is_there_a_winning_row(matrix: list[Any]) -> tuple:
+def get_a_winning_row_and_symbol(matrix: list[Any]) -> tuple:
     """
-    Checks whether there are only one type of symbols: 'X' or 'O' in a row or the row is not empty.
+    Checks whether there are only one type of symbols: 'X' or 'O' in a row or whether the row is not empty.
     :param matrix: A game matrix with current state of game.
     :return: A number of winning rows and a winning symbol.
     """
@@ -36,10 +41,75 @@ def is_there_a_winning_row(matrix: list[Any]) -> tuple:
     return number_of_winning_rows, winning_symbol
 
 
+def split_and_validate_coordinates(coordinates: str) -> tuple[int, int]:
+    try:
+        y, x = coordinates.split()
+    except ValueError:
+        raise WrongCoordinates("You should enter numbers!")
+
+    if x.isnumeric() and y.isnumeric():
+        x = int(x)
+        y = int(y)
+    else:
+        raise WrongCoordinates("You should enter numbers!")
+
+    if x not in (1, 2, 3) or y not in (1, 2, 3):
+        raise WrongCoordinates("Coordinates should be from 1 to 3!")
+    return y, x
+
+
+def search_possible_move_coordinates(matrix: list[list[str]]) -> list[tuple[int, int]]:
+    empty_cells_coordinates: list[tuple[int, int]] = []
+    for x_index, line in enumerate(matrix):
+        for y_index, cell in enumerate(line):
+            if cell == " ":
+                empty_cells_coordinates.append((y_index + 1, x_index + 1))
+    return empty_cells_coordinates
+
+
+def check_game_result(matrix: list[list[str]]) -> str:
+    x_quantity: int = 0
+    o_quantity: int = 0
+    empty_cell_quantity: int = 0
+
+    # rotates (transposes) the matrix for easier iterations through columns with for loops
+    matrix_transposed: list[tuple[Any]] = list(zip(*matrix))
+
+    # contains the diagonals of the game matrix
+    matrix_diagonals: list[list[str]] = [
+        [matrix[0][0], matrix[1][1], matrix[2][2]],
+        [matrix[0][2], matrix[1][1], matrix[2][0]]
+    ]
+
+    winning_rows, winning_row_symbol = get_a_winning_row_and_symbol(matrix)
+
+    winning_columns, winning_column_symbol = get_a_winning_row_and_symbol(matrix_transposed)
+
+    winning_diagonals, winning_diagonal_symbol = get_a_winning_row_and_symbol(matrix_diagonals)
+
+    for line in matrix:
+        x_quantity += line.count("X")
+        o_quantity += line.count("O")
+        empty_cell_quantity += line.count(" ")
+
+    # checks whether there aren't too many symbols of one type or more than one winning rows
+    if abs(x_quantity - o_quantity) > 1 or winning_rows > 1 or winning_columns > 1:
+        whose_turn.reverse()
+        return "Impossible"
+    elif winning_rows == 0 and winning_columns == 0 and winning_diagonals == 0 and empty_cell_quantity == 0:
+        return "Draw"
+
+    if winning_rows == 1:
+        return f"{winning_row_symbol} wins"
+    elif winning_columns == 1:
+        return f"{winning_column_symbol} wins"
+    elif winning_diagonals == 1:
+        return f"{winning_diagonal_symbol} wins"
+    elif empty_cell_quantity > 0:
+        return ""
+
+
 if __name__ == '__main__':
-    global X_quantity
-    global O_quantity
-    global empty_cell_quantity
     whose_turn: list = ["X", "O"]
 
     game_matrix: list[list[str]] = [
@@ -47,94 +117,34 @@ if __name__ == '__main__':
         [" ", " ", " "],
         [" ", " ", " "]
     ]
-    while True:
-        starting_table: str = input("Please enter starting table: ")
-        if len(starting_table) != STARTING_TABLE_LEN:
-            print("Wrong length of the starting table!")
-            continue
-        elif set(starting_table) != GAME_SYMBOLS:
-            print("Wrong symbols in the starting table!")
-            continue
-        else:
-            # splitting string into a 3x3 matrix
-            starting_table = starting_table.replace("_", " ")
-            game_matrix = [list(starting_table[x:x+3]) for x in range(0, len(starting_table), 3)]
-            print(game_table(game_matrix))
-            break
+
+    print(game_table(game_matrix))
 
     while True:
         while True:
-            move_coordinates: str = input("Please enter coordinates of your move (row column): ")
-            try:
-                y, x = move_coordinates.split()
-            except ValueError:
-                print("You should enter numbers!")
-                continue
-
-            if x.isnumeric() and y.isnumeric():
-                x = int(x)
-                y = int(y)
+            possible_moves = search_possible_move_coordinates(game_matrix)
+            if whose_turn[0] == "X":
+                move_coordinates: str = input("Enter the coordinates: ")
+                try:
+                    y, x = split_and_validate_coordinates(move_coordinates)
+                except WrongCoordinates as error:
+                    print(error)
+                    continue
             else:
-                print("You should enter numbers!")
-                continue
+                y, x = random.choice(possible_moves)
+                print('Making move level "easy"')
 
-            if x not in (1, 2, 3) or y not in (1, 2, 3):
-                print("Coordinates should be from 1 to 3!")
-                continue
-
-            if game_matrix[y - 1][x - 1] == "X" or game_matrix[y - 1][x - 1] == "O":
+            if (y, x) not in possible_moves:
                 print("This cell is occupied! Choose another one!")
             else:
-                X_quantity: int = 0
-                O_quantity: int = 0
-                empty_cell_quantity: int = 0
-
-                for line in game_matrix:
-                    X_quantity += line.count("X")
-                    O_quantity += line.count("O")
-                    empty_cell_quantity += line.count(" ")
-
-                if X_quantity > O_quantity:
-                    whose_turn.reverse()
-
                 game_matrix[y - 1][x - 1] = whose_turn[0]
                 print(game_table(game_matrix))
                 break
 
-        # rotates (transposes) the matrix for easier iterations through columns with for loops
-        game_matrix_transposed: list[tuple[Any]] = list(zip(*game_matrix))
+        game_result = check_game_result(game_matrix)
 
-        # contains the diagonals of the game matrix
-        game_matrix_diagonals: list[list[str]] = [
-            [game_matrix[0][0], game_matrix[1][1], game_matrix[2][2]],
-            [game_matrix[0][2], game_matrix[1][1], game_matrix[2][0]]
-        ]
-
-        winning_rows, winning_row_symbol = is_there_a_winning_row(game_matrix)
-
-        winning_columns, winning_column_symbol = is_there_a_winning_row(game_matrix_transposed)
-
-        winning_diagonals, winning_diagonal_symbol = is_there_a_winning_row(game_matrix_diagonals)
-
-        # checks whether there aren't too many symbols of one type or more than one winning rows
-        if abs(X_quantity - O_quantity) > 1 or winning_rows > 1 or winning_columns > 1:
-            print("Impossible")
+        if game_result:
+            print(game_result)
+            break
+        else:
             whose_turn.reverse()
-            break
-        elif winning_rows == 0 and winning_columns == 0 and winning_diagonals == 0 and empty_cell_quantity == 0:
-            print("Draw")
-            break
-
-        if winning_rows == 1:
-            print(f"{winning_row_symbol} wins")
-            break
-        elif winning_columns == 1:
-            print(f"{winning_column_symbol} wins")
-            break
-        elif winning_diagonals == 1:
-            print(f"{winning_diagonal_symbol} wins")
-            break
-        elif empty_cell_quantity > 0:
-            print("Game not finished")
-
-        whose_turn.reverse()
